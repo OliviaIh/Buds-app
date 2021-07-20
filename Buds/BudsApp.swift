53a5f53d2e6679e9a18d879daa50e34af223f326
Olivia Ih <ih.olivia.e@gmail.com>
2021-07-19 19:53:43 -0700

    blank project

---
+++ Buds/BudsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudsApp.swift
+//  Buds
+//
+//  Created by Olivia Ih on 7/19/21.
+//
+
+import SwiftUI
+
+@main
+struct BudsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Buds/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Buds
+//
+//  Created by Olivia Ih on 7/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Buds")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
