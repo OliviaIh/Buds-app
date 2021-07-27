8e34ec4cc88d7b107488dd4d1ddc53b0e12fc67f
Olivia Ih <ih.olivia.e@gmail.com>
2021-07-26 23:35:43 -0700

    Firebase setup

--- Buds/BudsApp.swift
+++ Buds/BudsApp.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct BudsApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+        [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
 }
