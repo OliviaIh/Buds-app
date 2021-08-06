22a4b9eed0d1641ca33f72ba6de760a8a4039bb4
Olivia Ih <ih.olivia.e@gmail.com>
2021-08-06 07:47:17 -0700

    Initial draft of Messages page

--- Buds/Custom Views/MessagesView.swift
+++ Buds/Custom Views/MessagesView.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct MessagesView: View {
+    
+    @State private var search:String = ""
+        
     var body: some View {
-        Text("messages view")
+        VStack {
+            VStack {
+                // title and write message button
+                HStack {
+                    TopLeftTitle(title: "messages")
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "square.and.pencil")
+                            .foregroundColor(Color("Dark Teal"))
+                            .padding([.top, .trailing])
+                            .font(.largeTitle)
+                    })
+                }
+                
+                // search bar
+                SearchBar(searchInquiry: $search)
+            }
+            
+            
+            // messages list
+            ScrollView {
+                ForEach(0..<100) { num in
+                    Text("\(num)")
+                }
+            }
+        }
+        .navigationBarTitle("")
+        .navigationBarHidden(true)
     }
 }
 
